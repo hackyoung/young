b72907c71ba45a4d7453201ab77004799ac4ec72
young <hackyoung110@gmail.com>
2014-06-20 15:59:17 +0800

    test

---
+++ js/_object.js
@@ -0,0 +1,24 @@
+/*
+ *  this is the young's base class
+ *  the root object of the packege
+ *  parent:
+ *  	base
+ *
+ *  method:
+ *  	belong: belong to who
+ *  	css: load the css for the object
+ *  	getText: the text is someword the display for user
+ *  	setText:
+ *  	
+ */
+
+/*
+ * AUTHOR: YOUNG
+ * MAIL: hackyoung110@gmail.com
+ * GPL PROGRAM
+ */
+
+function _object() {
+	var _this = this;
+	
+}
